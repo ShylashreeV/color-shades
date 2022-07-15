2c1f5417a74b3d5b25be5265fc1d5521f0194c33
Shylasonu <98378861+Shylasonu@users.noreply.github.com>
2022-07-15 21:56:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import "./App.css";
-import ChromeColor from '@uiw/react-color-chrome';
-import Values from 'values.js';
+import ChromeColor from "@uiw/react-color-chrome";
+import Values from "values.js";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
-  const [color, setColor] = useState('#ccc');
+  const [color, setColor] = useState("#ccc");
   const [shades, setShades] = useState([]);
   const [showPicker, setShowPicker] = useState(false);
 
@@ -18,33 +18,39 @@ function App() {
   };
 
   useEffect(() => {
-    onChange({hex: '#ccc'});
+    onChange({ hex: "#ccc" });
   }, []);
 
   return (
-    <div className='wrapper'>
-      <h1 className='heading'> COLOR SHADES GENERATOR <span onClick={() => setShowPicker(!showPicker)}>({color})</span>  </h1>
-      <div className='color-picker'>
-        {
-        showPicker && <ChromeColor color={color} onChange={onChange}/>
-        }
+    <div className="wrapper">
+      <h1 className="heading">
+        {" "}
+        COLOR SHADES GENERATOR{" "}
+        <span onClick={() => setShowPicker(!showPicker)}>({color})</span>{" "}
+      </h1>
+      <div className="color-picker">
+        {showPicker && <ChromeColor color={color} onChange={onChange} />}
       </div>
-        <div className='shades'>
-          <ul className='shades-list'>
-            {
-              shades.map(shade => {
-                return (<CopyToClipboard className="color-code" text={"#" + shade.hex}>
-                    <li style={{ backgroundColor: "#" + shade.hex, color:shade.type === 'shade' ? 'white' : 'black' }}>
-                      {shade.hex}
-                    </li>
-                  </CopyToClipboard>
-                );
-              })
-            }
-          </ul>
-        </div>
+      <div className="shades">
+        <ul className="shades-list">
+          {shades.map((shade) => {
+            return (
+              <CopyToClipboard className="color-code" text={"#" + shade.hex}>
+                <li
+                  style={{
+                    backgroundColor: "#" + shade.hex,
+                    color: shade.type === "shade" ? "white" : "black",
+                  }}
+                >
+                  {shade.hex}
+                </li>
+              </CopyToClipboard>
+            );
+          })}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
